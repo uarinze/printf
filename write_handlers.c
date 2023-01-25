ba59814f9d013ed23277875076c576cbd0aab43a
ijeoruche <nwanzeij@gmail.com>
2023-01-24 18:29:10 -0800

    update

--- write_handlers.c
+++ write_handlers.c
@@ -30,7 +30,7 @@ int handle_write_char(char c, char buffer[], int flags,
 		for (i = 0; i < width - 1; i++)
 			buffer[BUFF_SIZE - i - 2] = padd;
 		if (flags & F_MINUS)
-			return (write(1, &buffer[0]) +
+			return (write(1, &buffer[0], 1) +
 					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
 			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
@@ -56,8 +56,7 @@ int write_number(int is_negative, int ind, char buffer[],
 		int flags, int width, int precision, int size)
 {
 	int length = BUFF_SIZE - ind - 1;
-	char padd = ' ';
-	char extra_ch = 0;
+	char padd = ' ', extra_ch = 0;
 
 	UNUSED(size);
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
